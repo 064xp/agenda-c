12b704234a708a3b47efce18f51262db09d067c1
Paulo <paulozamora2000@gmail.com>
2019-11-03 22:26:05 -0600

    Implmentacion lectura y escritura del array de structs a archivo

--- src/agenda.c
+++ src/agenda.c
@@ -6,7 +6,7 @@ typedef struct pendiente { //formato de un solo pendiente
   int hora; //hora del pendiente en formato 24h
   int minuto;
   int completado; //si ya esta completada, sera 1
-  char contenido[40];
+  char contenido[60];
 } pendiente;
 
 typedef struct fecha { //fecha del dia actual
@@ -21,6 +21,8 @@ void menuPrincipal(pendiente[], int, char[], fecha);
 void imprimirPendientesDelDia(struct pendiente[], int);
 void fijarFecha(fecha*);
 void gotoxy(int, int);
+int escribirArchivo(pendiente *, int);
+pendiente *leerArchivo(int *, char *);
 
 void generarPendientes(pendiente pendientesDeHoy[]);
 
@@ -28,15 +30,21 @@ int main(){
   fecha fechaDeHoy;
   char opcion[30] = ""; //cuando el usuario indique que quiere salir, este sera "-1"
   fijarFecha(&fechaDeHoy);
-  pendiente pendientesDeHoy[5];
-  int cantidadDePendientes = 5;
-  generarPendientes(pendientesDeHoy);
+  pendiente *pendientesDeHoy;
+  int cantidadDePendientes;
+
+  pendientesDeHoy = leerArchivo(&cantidadDePendientes, "");
+  if(pendientesDeHoy == NULL){
+    printf("Ocurrio un error al intentar abrir el archivo\n");
+    exit(1);
+  }
 
   system("mode con: cols=100 lines=30"); //establecer dimensiones de la ventana
   // loop central del programa
   while(strcmp(opcion, "-1") != 0){
     menuPrincipal(pendientesDeHoy, cantidadDePendientes, opcion, fechaDeHoy);
   }
+  free(pendientesDeHoy);
   return 0;
 }
 
@@ -48,6 +56,22 @@ void menuPrincipal(pendiente pendientesDeHoy[], int cantidadDePendientes, char o
   scanf("%s", opcion); //no usamos & ya que los arrays se pasan por referencia por default
   system("cls");
 }
+
+//funcion de prueba para fines de desarrollo
+void generarPendientes(pendiente pendientesDeHoy[]){
+  int i;
+  for(i=0; i<5; i++){
+    pendientesDeHoy[i].hora = i + 15;
+    pendientesDeHoy[i].minuto = i*2 + 20;
+    if(i == 2 || i == 4){
+      pendientesDeHoy[i].completado = 1;
+    } else{
+      pendientesDeHoy[i].completado = 0;
+    }
+    sprintf(pendientesDeHoy[i].contenido, "Pendiente de prueba no. #%i", i);
+  }
+}
+
 
 void fijarFecha(fecha *fechaActual){
   char diaSemana[15];
@@ -81,17 +105,35 @@ void gotoxy(int x, int y){
 	printf("\033[%d;%dH", y, x);
 }
 
-//esta funcion es solo para prueba durante el desarrollo
-void generarPendientes(pendiente pendientesDeHoy[]){
-  int i;
-  for(i=0; i<5; i++){
-    pendientesDeHoy[i].hora = i + 15;
-    pendientesDeHoy[i].minuto = i*2 + 20;
-    if(i == 2 || i == 4){
-      pendientesDeHoy[i].completado = 1;
-    } else{
-      pendientesDeHoy[i].completado = 0;
-    }
-    sprintf(pendientesDeHoy[i].contenido, "Pendiente de prueba no. #%i", i);
+int escribirArchivo(pendiente *pendientes, int cantidadDePendientes){
+  FILE *fp;
+  fp = fopen("agenda.dat", "wb");
+  if(fp != NULL){
+    fwrite(pendientes, sizeof(pendiente), cantidadDePendientes, fp);
+    fclose(fp);
+    return 0;
+  } else {
+    return 1;
   }
+}
+
+pendiente *leerArchivo(int *cantidadDePendientes, char *nombreDelArchivo){
+  FILE *fp;
+  int tamanoDelArchivo;
+  pendiente *buffer;
+  nombreDelArchivo = nombreDelArchivo[0] == '\0' ? "agenda.dat" : nombreDelArchivo;
+
+  fp = fopen(nombreDelArchivo, "rb");
+  if(fp != NULL){
+    fseek(fp, 0L, SEEK_END);
+    tamanoDelArchivo = ftell(fp);
+    *cantidadDePendientes = tamanoDelArchivo / sizeof(pendiente);
+    rewind(fp);
+    buffer = malloc(tamanoDelArchivo);
+    fread(buffer, sizeof(pendiente), *cantidadDePendientes, fp);
+    fclose(fp);
+  } else {
+    return NULL;
+  }
+  return buffer;
 }
